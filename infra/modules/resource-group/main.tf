62656790e7bead1b27c87d329ce1b2c08f5e6b7b
etnmn <etienne@menou.fr>
2026-01-20 22:41:20 +0000

    ci: add reusable tflint workflow

--- infra/modules/resource-group/main.tf
+++ infra/modules/resource-group/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.14.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.57"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "this" {
   name     = var.name
   location = var.location
