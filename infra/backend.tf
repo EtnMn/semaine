5bb453bbf1f09323888b7db155b96cd42bed24d9
etnmn <etienne@menou.fr>
2026-06-09 20:23:58 +0000

    chore: update terraform backend configuration

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-etn-semaine-tfstate"
-    storage_account_name = "stetnsemaine32658"
+    storage_account_name = "stetnsemaine25390"
     container_name       = "tfstateprod"
     key                  = "semaine.tfstate"
   }
